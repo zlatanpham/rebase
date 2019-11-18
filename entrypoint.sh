62952c1205114c8d6cec866243c8a307823f70c9
fedor <fedor.korotkov@gmail.com>
2019-11-17 22:24:11 -0500

    reformat

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,16 +15,16 @@ echo "Checking if issue is a pull request..."
 (jq -r ".issue.pull_request.url" "$GITHUB_EVENT_PATH") || exit $NEUTRAL_EXIT_CODE
 
 if [[ "$(jq -r ".action" "$GITHUB_EVENT_PATH")" != "created" ]]; then
-	echo "This is not a new comment event!"
-	exit $NEUTRAL_EXIT_CODE
+  echo "This is not a new comment event!"
+  exit $NEUTRAL_EXIT_CODE
 fi
 
 PR_NUMBER=$(jq -r ".issue.number" "$GITHUB_EVENT_PATH")
 echo "Collecting information about PR #$PR_NUMBER of $GITHUB_REPOSITORY..."
 
 if [[ -z "$GITHUB_TOKEN" ]]; then
-	echo "Set the GITHUB_TOKEN env variable."
-	exit 1
+  echo "Set the GITHUB_TOKEN env variable."
+  exit 1
 fi
 
 URI=https://api.github.com
@@ -32,20 +32,20 @@ API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 
 pr_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
-          "${URI}/repos/$GITHUB_REPOSITORY/pulls/$PR_NUMBER")
+  "${URI}/repos/$GITHUB_REPOSITORY/pulls/$PR_NUMBER")
 
 BASE_REPO=$(echo "$pr_resp" | jq -r .base.repo.full_name)
 BASE_BRANCH=$(echo "$pr_resp" | jq -r .base.ref)
 
 if [[ "$(echo "$pr_resp" | jq -r .rebaseable)" != "true" ]]; then
-	echo "GitHub doesn't think that the PR is rebaseable!"
-	exit 1
+  echo "GitHub doesn't think that the PR is rebaseable!"
+  exit 1
 fi
 
 if [[ -z "$BASE_BRANCH" ]]; then
-	echo "Cannot get base branch information for PR #$PR_NUMBER!"
-	echo "API response: $pr_resp"
-	exit 1
+  echo "Cannot get base branch information for PR #$PR_NUMBER!"
+  echo "API response: $pr_resp"
+  exit 1
 fi
 
 HEAD_REPO=$(echo "$pr_resp" | jq -r .head.repo.full_name)
@@ -54,8 +54,8 @@ HEAD_BRANCH=$(echo "$pr_resp" | jq -r .head.ref)
 echo "Base branch for PR #$PR_NUMBER is $BASE_BRANCH"
 
 if [[ "$BASE_REPO" != "$HEAD_REPO" ]]; then
-	echo "PRs from forks are not supported at the moment."
-	exit 1
+  echo "PRs from forks are not supported at the moment."
+  exit 1
 fi
 
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
@@ -73,7 +73,7 @@ git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
 
 # Do an exact check instead of `rebase *` so it's not possible to inject malisios commands
 if [[ $(jq -r ".comment.body" "$GITHUB_EVENT_PATH" | grep -Fq "/rebase --autosquash") -eq 0 ]]; then
-	git rebase --autosquash origin/$BASE_BRANCH
+  git rebase --autosquash origin/$BASE_BRANCH
 else
   git rebase origin/$BASE_BRANCH
 fi
